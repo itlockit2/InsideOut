099e024326b51da4fe35574eb82216c88f5bcaba
itlockit2 <31428960+itlockit2@users.noreply.github.com>
2017-12-04 00:29:11 +0900

    GameSightLimit 리팩토링

--- oneTrillion/src/project_04/GameSightLimit.java
+++ oneTrillion/src/project_04/GameSightLimit.java
@@ -14,10 +14,6 @@ public class GameSightLimit {
 	
 	/** 게임 진행중 시야 제한 이벤트를 위한 이미지 */
 	private Image gameSightLimitImage;
-	/** Screen 제한의 출현위치 x좌표를 나타내는 필드값 */
-	private int x;
-	/** Screen 제한의 출현위치 y좌표를 나타내는 필드값 */
-	private int y;
 	/** 시야제한 이벤트를 시작하는 시간을 나타내는 필드값*/
 	private long startTime;
 	/** 시야제한 이벤트를 끝내는 지점을 나타내는 필드값 */
@@ -32,11 +28,6 @@ public class GameSightLimit {
       	
 		gameSightLimitImage = new ImageIcon(getClass().getClassLoader().getResource("images/sightLimitImage.png"))
 				.getImage();
-		
-	    // x좌표
-		this.x = x ;
-	    // y좌표
-		this.y = y;
 		// 시야를 제한 할 시작 시간
 		this.startTime = startTime;
 		// 시야 제한을 끝낼 시간
@@ -47,14 +38,6 @@ public class GameSightLimit {
 	/** 시야제한 이미지를 리턴해주는 메소드이다. */
 	public Image getGameSightLimitImage() {
 		return gameSightLimitImage;
-	}
-	/** 시야제한 이미지의 x좌표를 리턴해주는 메소드이다. */
-	public int getX() {
-		return x;
-	}
-	/** 시야제한 이미지의 y좌표를 리턴해주는 메소드이다. */
-	public int getY() {
-		return y;
 	}
 	/** 시야 제한 이미지의 시작시간을 리턴해주는 메소드이다. */
 	public long getStartTime() {
